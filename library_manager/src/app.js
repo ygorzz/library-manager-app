b5e0140c979deee48f7f21c9782b862d6c144276
Ygor Santos <santosygor2007@gmail.com>
2026-03-30 14:54:56 -0300

    Atualização da estrutura de aqruivos

--- library_manager/src/app.js
+++ library_manager/src/app.js
@@ -7,7 +7,7 @@ const reader = createInterface({
     output: process.stdout
 })
 
-const caminhoArquivo = './livros.json'
+const caminhoArquivo = './src/livros.json'
 
 // Por ser um projeto pequeno não me preocupei com risco de stack overflow por causa das recursões
 export function mostrarMenu() {
